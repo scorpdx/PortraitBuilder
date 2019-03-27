1935bd9b9ddcb32d5bbdde3e1b30d78d5ab14dde
J. Zebedee <zebedee@code.gripe>
2019-03-27 08:30:46 -0500

    Working full portrait rendering

--- src/PortraitBuilder.Online/Portrait.cs
+++ src/PortraitBuilder.Online/Portrait.cs
@@ -8,14 +8,35 @@ using PortraitBuilder.Model.Portrait;
 using PortraitBuilder.Engine;
 using PortraitBuilder.Model;
 using SkiaSharp;
+using System.Linq;
+using PortraitBuilder.Model.Content;
+using System;
 
 namespace PortraitBuilder.Online
 {
     public static class PortraitFunction
     {
+        private static readonly Lazy<Loader> _loader = new Lazy<Loader>(() =>
+        {
+            var user = new User
+            {
+                GameDir = @"X:\Games\Steam\steamapps\common\Crusader Kings II",//readGameDir();
+                ModDir = @"C:\Users\scorp\Documents\Paradox Interactive\Crusader Kings II",//readModDir(user.GameDir);
+                DlcDir = "dlc/"
+            };
+
+            var loader = new Loader(user);
+            loader.LoadVanilla();
+            var dlcs = loader.LoadDLCs();
+            loader.UpdateActiveAdditionalContent(dlcs.Where(dlc => dlc.HasPortraitData).Cast<Content>().ToList());
+            loader.LoadPortraits();
+
+            return loader;
+        });
+
         [FunctionName("portrait")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -25,34 +46,43 @@ namespace PortraitBuilder.Online
             if (string.IsNullOrEmpty(dna) || string.IsNullOrEmpty(properties))
                 return new BadRequestResult();
 
-            string customProperties = req.Query["customProperties"];
+            var ptBase = req.Query["base"];
+            if (string.IsNullOrEmpty(ptBase))
+                return new BadRequestResult();
+
+            var ptClothing = req.Query["clothing"];
+            var government = req.Query["government"];
+            var titleRank = req.Query["titleRank"];
 
             var character = new Character();
-            character.Import(dna, properties + customProperties);
+            character.Import(dna, properties);
 
-            // Reflect on dropdown
-            //updateSelectedCharacteristicValues(portrait);
-
-            //started = true;
-
-            var portraitRenderer = new PortraitRenderer();
-            User user = new User
+            if(!string.IsNullOrEmpty(government) && Enum.TryParse(government, true, out GovernmentType gov))
             {
-                GameDir = @"X:\Games\Steam\steamapps\common\Crusader Kings II",//readGameDir();
-                ModDir = @"C:\Users\scorp\Documents\Paradox Interactive\Crusader Kings II",//readModDir(user.GameDir);
-                DlcDir = "dlc/"
-            };
-            var loader = new Loader(user);
-            loader.LoadVanilla();
-            loader.LoadPortraits();
+                character.Government = gov;
+            }
+
+            if(!string.IsNullOrEmpty(titleRank) && Enum.TryParse(titleRank, true, out TitleRank rank))
+            {
+                character.Rank = rank;
+            }
+
+            var loader = _loader.Value;
             if (loader.ActivePortraitData.PortraitTypes.Count == 0)
             {
-                //logger.LogCritical("No portrait types found.");
-                //return;
+                return new StatusCodeResult(500);
             }
-            var selectedPortraitType = "andalusiangfx_male";
-            character.PortraitType = loader.GetPortraitType($"PORTRAIT_{selectedPortraitType}");
 
+            if (string.IsNullOrEmpty(ptClothing))
+            {
+                character.PortraitType = loader.GetPortraitType($"PORTRAIT_{ptBase}");
+            }
+            else
+            {
+                character.PortraitType = loader.GetPortraitType($"PORTRAIT_{ptBase}", $"PORTRAIT_{ptClothing}");
+            }
+
+            var portraitRenderer = new PortraitRenderer();
             var bmp = portraitRenderer.DrawCharacter(character, loader.ActiveContents, loader.ActivePortraitData.Sprites);
             var png = SKImage.FromBitmap(bmp).Encode();
 
