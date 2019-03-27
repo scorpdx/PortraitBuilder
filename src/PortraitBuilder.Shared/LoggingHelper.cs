cd97ee778624453ba31c2dec3e690b807145bbf0
J. Zebedee <zebedee@code.gripe>
2019-03-27 03:51:16 -0500

    Add MSFT logging abstractions

---
+++ src/PortraitBuilder.Shared/LoggingHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PortraitBuilder
+{
+    internal static class LoggingHelper
+    {
+        internal static ILoggerFactory LoggerFactory { get; set; }
+        internal static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+        internal static ILogger CreateLogger(string categoryName) => LoggerFactory.CreateLogger(categoryName);
+    }
+}
