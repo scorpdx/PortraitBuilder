fcec46041a55f6691b69badc3851dd0f8d654297
J. Zebedee <zebedee@code.gripe>
2019-03-28 08:17:15 -0500

    Remove ActiveContent from ISpriteCache

--- src/PortraitBuilder.Shared/Engine/ISpriteCache.cs
+++ src/PortraitBuilder.Shared/Engine/ISpriteCache.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections.Generic;
-using PortraitBuilder.Model.Content;
-using PortraitBuilder.Model.Portrait;
+﻿using PortraitBuilder.Model.Portrait;
 
 namespace PortraitBuilder.Engine
 {
     public interface ISpriteCache
     {
-        IReadOnlyList<Content> ActiveContent { get; }
-
         Sprite Get(SpriteDef def);
     }
 }
