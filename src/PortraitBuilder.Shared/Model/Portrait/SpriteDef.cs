5d7dedec248ff68c54042fae05950a6617b5710c
J. Zebedee <zebedee@code.gripe>
2019-03-28 04:10:08 -0500

    Add SpriteDef

---
+++ src/PortraitBuilder.Shared/Model/Portrait/SpriteDef.cs
@@ -0,0 +1,11 @@
+﻿namespace PortraitBuilder.Model.Portrait
+{
+    //TODO: make immutable
+    public sealed class SpriteDef
+    {
+        public string Name { get; set; }
+        public string TextureFilePath { get; set; }
+        public int FrameCount { get; set; }
+        public bool NoRefCount { get; set; }
+    }
+}
