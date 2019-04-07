def968922b1020a7dbe0aa15cd3de3d04d7eddd6
J. Zebedee <zebedee@code.gripe>
2019-04-07 08:41:28 -0500

    Add PortraitBuilder

---
+++ src/PortraitBuilder.Shared/Engine/PortraitBuilder.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Extensions.Logging;
+using PortraitBuilder.Model;
+using PortraitBuilder.Model.Portrait;
+using System.Collections.Generic;
+
+namespace PortraitBuilder.Engine
+{
+    public class PortraitBuilder
+    {
+        private static readonly ILogger logger = LoggingHelper.CreateLogger<PortraitRenderer>();
+
+        private const string GovernmentSpritePrefix = "GFX_charframe_150";
+        private static IReadOnlyDictionary<GovernmentType, string> GovernmentSpriteNames { get; } = new Dictionary<GovernmentType, string>
+        {
+            { GovernmentType.Feudal, GovernmentSpritePrefix },
+            { GovernmentType.Iqta, $"{GovernmentSpritePrefix}_iqta"},
+            { GovernmentType.Theocracy, $"{GovernmentSpritePrefix}_theocracy"},
+            { GovernmentType.Republic, $"{GovernmentSpritePrefix}_republic"},
+            { GovernmentType.MerchantRepublic, $"{GovernmentSpritePrefix}_merchantrepublic"},
+            { GovernmentType.Tribal, $"{GovernmentSpritePrefix}_tribal"},
+            { GovernmentType.Nomadic, $"{GovernmentSpritePrefix}_nomadic"},
+            { GovernmentType.MonasticFeudal, $"{GovernmentSpritePrefix}_theocraticfeudal"},
+            { GovernmentType.ChineseImperial, $"{GovernmentSpritePrefix}_chineseimperial" },
+            { GovernmentType.ConfucianBureaucracy, $"{GovernmentSpritePrefix}_confucian" },
+        };
+
+        public class TileRenderStep
+        {
+            public SpriteDef Def;
+            public int TileIndex;
+            public SkiaSharp.SKPointI TileOffset;
+        }
+
+        public class HairRenderStep : TileRenderStep
+        {
+            public Hair Hair;
+        }
+
+        public class EyeRenderStep : TileRenderStep
+        {
+            public SkiaSharp.SKColor EyeColor;
+        }
+
+        public IEnumerable<TileRenderStep> BuildCharacter(Character character, Dictionary<string, SpriteDef> sprites)
+        {
+            foreach (var layer in character.PortraitType.Layers)
+            {
+                yield return BuildLayer(layer, character, sprites);
+            }
+
+            yield return BuildBorder(character, sprites);
+        }
+
+        private TileRenderStep? BuildLayer(Layer layer, Character character, Dictionary<string, SpriteDef> sprites)
+        {
+            // Backup for merchants, which are part of "The Republic" DLC !
+            string spriteName = GetOverriddenSpriteName(character, layer);
+            if (!sprites.TryGetValue(spriteName, out SpriteDef def) && !sprites.TryGetValue(layer.Name, out def))
+                return null;
+
+            //Get DNA/Properties letter, then the index of the tile to draw
+            if (!TryGetTileIndex(character, def.FrameCount, layer, out int tileIndex))
+                return null;
+
+            return BuildTile(character, layer, def, tileIndex);
+        }
+
+        private string GetOverriddenSpriteName(Character character, Layer layer)
+        {
+            string spriteName = layer.Name;
+
+            var hasSpecialGovernment = character.Government == GovernmentType.Theocracy || character.Government == GovernmentType.MerchantRepublic;
+            var isOutfitLayer = layer.Characteristic == DefaultCharacteristics.CLOTHES || layer.Characteristic == DefaultCharacteristics.HEADGEAR;
+
+            if (hasSpecialGovernment && isOutfitLayer)
+            {
+                string sex = character.Sex == Sex.Male ? "male" : "female";
+                string layerSuffix = spriteName.Contains("behind") ? "_behind" : ""; // Handles clothes_infront and headgear_mid
+                string government = character.Government == GovernmentType.Theocracy ? "religious" : "merchant";
+                string layerType = layer.Characteristic == DefaultCharacteristics.CLOTHES ? "clothes" : "headgear";
+                spriteName = $"GFX_{government}_{sex}_{layerType}{layerSuffix}";
+            }
+
+            return spriteName;
+        }
+
+        public TileRenderStep? BuildBorder(Character character, Dictionary<string, SpriteDef> sprites)
+        {
+            string governmentSpriteName = GovernmentSpriteNames[character.Government];
+            if (!sprites.TryGetValue(governmentSpriteName, out SpriteDef def))
+                return null;
+
+            return new TileRenderStep
+            {
+                Def = def,
+                TileIndex = (int)character.Rank,
+            };
+        }
+
+        private bool TryGetTileIndex(Character character, int frameCount, Layer layer, out int tileIndex)
+        {
+            tileIndex = default;
+            if (!character.TryGetLetter(layer.Characteristic, out char letter))
+            {
+                logger.LogWarning("Letter not found. character {0} layer {1} characteristic {2}", character, layer, layer.Characteristic);
+                return false;
+            }
+
+            tileIndex = Character.GetIndex(letter, frameCount);
+            logger.LogDebug($"Layer letter: {letter}, Tile Index: {tileIndex}");
+
+            return true;
+        }
+
+        private TileRenderStep? BuildTile(Character character, Layer layer, SpriteDef def, int tileIndex)
+        {
+            var tileOffset = new SkiaSharp.SKPointI(12 + layer.Offset.X, 12 + 152 /*- tile.Height*/ - layer.Offset.Y);
+            if (layer.IsHair)
+            {
+                var hairColors = character.PortraitType.HairColours;
+                if (!character.TryGetLetter(DefaultCharacteristics.HAIR_COLOR, out char hairChar))
+                {
+                    logger.LogError("Letter not found. character {0} characteristic {1}", character, DefaultCharacteristics.HAIR_COLOR);
+                    return null;
+                }
+
+                int hairIndex = Character.GetIndex(hairChar, hairColors.Count);
+                return new HairRenderStep
+                {
+                    Def = def,
+                    TileIndex = tileIndex,
+                    Hair = hairColors[hairIndex],
+                    TileOffset = tileOffset,
+                };
+            }
+            else if (layer.IsEye)
+            {
+                var eyeColors = character.PortraitType.EyeColours;
+                if (!character.TryGetLetter(DefaultCharacteristics.EYE_COLOR, out char eyeChar))
+                {
+                    logger.LogError("Letter not found. character {0} characteristic {1}", character, DefaultCharacteristics.EYE_COLOR);
+                    return null;
+                }
+
+                int eyeIndex = Character.GetIndex(eyeChar, eyeColors.Count);
+                return new EyeRenderStep
+                {
+                    Def = def,
+                    TileIndex = tileIndex,
+                    EyeColor = eyeColors[eyeIndex],
+                    TileOffset = tileOffset,
+                };
+            }
+            else
+            {
+                return new TileRenderStep
+                {
+                    Def = def,
+                    TileIndex = tileIndex,
+                    TileOffset = tileOffset,
+                };
+            }
+        }
+    }
+}
