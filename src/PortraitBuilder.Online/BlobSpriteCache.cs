cb2a5682bc3e5f7a6059cf7238d9e3116ef82bde
J. Zebedee <zebedee@code.gripe>
2019-04-07 10:12:01 -0500

    Interim commit of blob work

---
+++ src/PortraitBuilder.Online/BlobSpriteCache.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using PortraitBuilder.Engine;
+using PortraitBuilder.Model.Content;
+using PortraitBuilder.Model.Portrait;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PortraitBuilder.Online
+{
+    public class BlobSpriteCache : ISpriteCache
+    {
+        private static readonly ILogger logger = LoggingHelper.CreateLogger<BlobSpriteCache>();
+
+        private readonly ConcurrentDictionary<SpriteDef, BlobSprite> _sprites;
+
+        private readonly CloudBlobContainer _pack;
+
+        public BlobSpriteCache(CloudBlobContainer pack)
+        {
+            _pack = pack;
+            _sprites = new ConcurrentDictionary<SpriteDef, BlobSprite>();
+        }
+
+        public BlobSprite Get(SpriteDef def) => _sprites?.GetOrAdd(def, LoadSprite) ?? throw new ObjectDisposedException(nameof(BlobSpriteCache));
+        ISprite ISpriteCache.Get(SpriteDef def) => Get(def);
+
+        private Task<BlobSprite> LoadSpriteAsync(SpriteDef def)
+        {
+            var blobPath = def.TextureFilePath;
+            if (string.IsNullOrEmpty(blobPath))
+                throw new ArgumentException($"No texture path in sprite definition {def}", nameof(def));
+
+            var blob = _pack.GetBlockBlobReference(blobPath);
+            blob.
+            DirectoryInfo dir = null;
+            // Loop on reverse order - last occurence wins if asset is overriden !
+            for (int i = ActiveContent.Count - 1; i >= 0; i--)
+            {
+                var content = ActiveContent[i];
+                dir = new DirectoryInfo(Path.Combine(content.AbsolutePath, def.Name));
+
+                if(dir.Exists && dir.EnumerateFiles().Any()) break;
+            }
+
+            if (dir == null || !dir.Exists)
+                throw new FileNotFoundException(string.Format("Unable to find sprite: {0} under active content {1}", def.Name, ActiveContent));
+
+            logger.LogDebug("Loading pack sprite from: {0}", dir.FullName);
+            return new PackSprite(dir.FullName, def.FrameCount);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    var oldDict = _sprites;
+                    foreach (var sprite in oldDict.Values)
+                    {
+                        sprite.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose() => Dispose(true);
+        #endregion
+    }
+}
--- src/PortraitBuilder.Online/Portrait.cs
+++ src/PortraitBuilder.Online/Portrait.cs
@@ -13,30 +13,44 @@ using PortraitBuilder.Model.Content;
 using System;
 using Newtonsoft.Json;
 using PortraitBuilder.ContentPacks;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace PortraitBuilder.Online
 {
     public static class PortraitFunction
     {
-        private static readonly Lazy<PackLoader> _loader = new Lazy<PackLoader>(() =>
+        private static readonly Lazy<CloudBlobClient> _storageClient = new Lazy<CloudBlobClient>(() =>
         {
-            var packsPath = @"C:\Dropbox\Data\LocalRepos\scorpdx\PortraitBuilder\src\SpritePackGenerator\bin\Debug\netcoreapp3.0\packs";
-            var packDir = new System.IO.DirectoryInfo(packsPath);
+            string storageConnectionString = Environment.GetEnvironmentVariable("storageconnectionstring");
+            var storage = CloudStorageAccount.Parse(storageConnectionString);
+            return storage.CreateCloudBlobClient();
+        });
+
+        private static readonly Lazy<ValueTask<PortraitData>> _portraitPack = new Lazy<PortraitData>(() =>
+        {
+            var client = _storageClient.Value;
+            var container = client.GetContainerReference("packs");
+            var blob = container.GetBlockBlobReference("portraits.json");
+
+            var packsPath = @"C:\Dropbox\Data\LocalRepos\scorpdx\PortraitBuilder\src\SpritePackGenerator\bin\Debug\netcoreapp3.0\packs";//Path.Combine("../", "packs/");
+            var packDir = new DirectoryInfo(packsPath);
 
             var skiaConv = new SkiaConverter();
-            var packContents = packDir.EnumerateFiles("*.json", System.IO.SearchOption.AllDirectories)
+            var packContents = packDir.EnumerateFiles("*.json", SearchOption.AllDirectories)
                 .Select(fi => fi.FullName)
-                .Select(System.IO.File.ReadAllText)
-                .Select(json => JsonConvert.DeserializeObject<Content>(json, skiaConv))
-                .ToArray();
-
-            foreach(var pack in packContents)
-            {
-                pack.AbsolutePath = System.IO.Path.Combine(@"C:\Dropbox\Data\LocalRepos\scorpdx\PortraitBuilder\src\SpritePackGenerator\bin\Debug\netcoreapp3.0\", pack.AbsolutePath);
-            }
+                .Select(File.ReadAllText)
+                .Select(json => JsonConvert.DeserializeObject<Content>(json, skiaConv));
 
             var loader = new PackLoader();
-            loader.ActiveContent.AddRange(packContents);
+            foreach (var pack in packContents)
+            {
+                pack.AbsolutePath = Path.Combine(@"C:\Dropbox\Data\LocalRepos\scorpdx\PortraitBuilder\src\SpritePackGenerator\bin\Debug\netcoreapp3.0\", pack.AbsolutePath); //Path.Combine("../", pack.AbsolutePath);
+                LoggingHelper.DefaultLogger?.LogInformation("Loaded pack {0}", pack.Name);
+
+                loader.ActiveContent.Add(pack);
+            }
             loader.LoadPortraits();
             loader.InvalidateCache();
 
@@ -45,10 +59,9 @@ namespace PortraitBuilder.Online
 
         [FunctionName("portrait")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
         {
-            LoggingHelper.DefaultLogger = log;
+            //LoggingHelper.DefaultLogger = log;
 
             string dna = req.Query["dna"];
             string properties = req.Query["properties"];
@@ -90,6 +103,14 @@ namespace PortraitBuilder.Online
             {
                 character.PortraitType = loader.GetPortraitType($"PORTRAIT_{ptBase}", $"PORTRAIT_{ptClothing}");
             }
+
+            var portraitBuilder = new PortraitBuilder.Engine.PortraitBuilder();
+            var steps = portraitBuilder.BuildCharacter(character, loader.ActivePortraitData.Sprites)
+                .Where(s => s != null)
+                .ToArray();
+
+            var sprites = steps.ToDictionary(s => s.Def.Name, s => s.Def);
+            //Microsoft.WindowsAzure.Storage.Blob.blob
 
             var portraitRenderer = new PortraitRenderer();
             var bmp = portraitRenderer.DrawCharacter(character, loader.Cache, loader.ActivePortraitData.Sprites);
