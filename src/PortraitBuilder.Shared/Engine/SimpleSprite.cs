44b8cada3a42737543d6fef4c646237124a94c71
J. Zebedee <zebedee@code.gripe>
2019-04-07 10:42:26 -0500

    Add SimpleSprite and SimpleSpriteCache

---
+++ src/PortraitBuilder.Shared/Engine/SimpleSprite.cs
@@ -0,0 +1,52 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PortraitBuilder.Engine
+{
+    public class SimpleSprite : ISprite
+    {
+        private IReadOnlyList<SKBitmap> _bitmaps;
+        public SimpleSprite(IReadOnlyList<SKBitmap> bitmaps)
+        {
+            if (bitmaps == null)
+                throw new ArgumentNullException(nameof(bitmaps));
+
+            _bitmaps = bitmaps;
+        }
+
+        public SKBitmap this[int index] => _bitmaps[index];
+
+        public int Count => _bitmaps.Count;
+
+        public IEnumerator<SKBitmap> GetEnumerator() => _bitmaps.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    foreach(var bitmap in _bitmaps)
+                    {
+                        bitmap?.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ src/PortraitBuilder.Shared/Engine/SimpleSpriteCache.cs
@@ -0,0 +1,51 @@
+﻿using PortraitBuilder.Model.Portrait;
+using System;
+using System.Collections.Concurrent;
+
+namespace PortraitBuilder.Engine
+{
+    public class SimpleSpriteCache<T> : ISpriteCache where T : ISprite
+    {
+        private readonly Func<SpriteDef, T> _resolver;
+
+        private readonly ConcurrentDictionary<SpriteDef, T> _sprites = new ConcurrentDictionary<SpriteDef, T>();
+
+        public SimpleSpriteCache(Func<SpriteDef, T> resolver)
+        {
+            _resolver = resolver;
+        }
+
+        public T Get(SpriteDef def)
+        {
+            if (disposedValue)
+                throw new ObjectDisposedException(nameof(SimpleSpriteCache<T>));
+
+            return _sprites.GetOrAdd(def, _resolver);
+        }
+
+        ISprite ISpriteCache.Get(SpriteDef def) => Get(def);
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    var oldDict = _sprites;
+                    foreach (var sprite in oldDict.Values)
+                    {
+                        sprite.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose() => Dispose(true);
+        #endregion
+    }
+}
