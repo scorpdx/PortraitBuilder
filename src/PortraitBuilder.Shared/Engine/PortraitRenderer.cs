5a7b2913ab5fd8c2d98fcd6c18c19753a729e9fb
J. Zebedee <zebedee@code.gripe>
2019-10-30 06:03:07 -0500

    Fix shader rendering
    This is a constant source of issues... we really need to add testing around it.
    
    First off, ALL operations should be done on unpremul, since that's the native format of the DDS textures we read in.
    
    Secondly, we should treat all textures/tiles as immutable and only perform mutations on canvases or copied bitmaps.
    
    Shader issues are common and frustrating. We need to check that:
      * Shaders are applied (changing haircolor/eyecolor results in different output)
      * Shader bitmaps are correctly loaded and positioned (scattered lines indicate a bug where the shader logic was run without having the original texture copied first)
      * Shader colors are applied (jet black output indicates shading was done incorrectly -- premul issue?)

--- src/PortraitBuilder.Shared/Engine/PortraitRenderer.cs
+++ src/PortraitBuilder.Shared/Engine/PortraitRenderer.cs
@@ -18,10 +18,10 @@ namespace PortraitBuilder.Engine
         {
             var portraitInfo = new SKImageInfo(176, 176);
             var portraitImage = new SKBitmap(portraitInfo);
-
             using (var canvas = new SKCanvas(portraitImage))
             {
                 //must set transparent bg for unpremul -> premul
+                //use white transparent
                 canvas.Clear(SKColors.Transparent);
                 foreach (var step in steps)
                 {
@@ -33,18 +33,18 @@ namespace PortraitBuilder.Engine
         }
 
         public static SKBitmap DrawCosmeticStep(PortraitBuilder.TileRenderStep step)
-        {
-            switch (step)
+            => step switch
             {
-                case PortraitBuilder.HairRenderStep hairStep: return ShadeHair(step.Tile, hairStep.Hair);
-                case PortraitBuilder.EyeRenderStep eyeStep: return ShadeEye(step.Tile, eyeStep.EyeColor);
-                default: return step.Tile;
-            }
-        }
+                PortraitBuilder.HairRenderStep hairStep => ShadeHair(step.Tile, hairStep.Hair),
+                PortraitBuilder.EyeRenderStep eyeStep => ShadeEye(step.Tile, eyeStep.EyeColor),
+                _ => step.Tile,
+            };
 
         public static void DrawTile(SKCanvas canvas, PortraitBuilder.TileRenderStep step)
         {
-            SKBitmap tile = DrawCosmeticStep(step);
+            //FIXME: this causes AVE?
+            /*using */
+            var tile = DrawCosmeticStep(step);
             canvas.DrawBitmap(tile, step.TileOffset);
         }
 
@@ -53,27 +53,23 @@ namespace PortraitBuilder.Engine
         /// </summary>
         public static SKBitmap ShadeEye(SKBitmap source, SKColor eyeColor)
         {
-            var output = new SKBitmap(source.Width, source.Height, source.ColorType, source.AlphaType);
-            output.Erase(SKColors.Transparent);
-
-            Debug.Assert(source.BytesPerPixel == 4 /* sizeof(BGRA) */);
+            var output = source.Copy();
 
             unsafe
             {
-                SKPMColor* sColor = (SKPMColor*)source.GetPixels().ToPointer();
-                SKPMColor* oColor = (SKPMColor*)output.GetPixels().ToPointer();
+                SKColor* oColor = (SKColor*)output.GetPixels().ToPointer();
 
                 for (int y = 0; y < source.Height; y++)
                 {
-                    for (int x = 0; x < source.Width; x++, sColor++, oColor++)
+                    for (int x = 0; x < source.Width; x++, oColor++)
                     {
-                        if (sColor->Alpha == 0) continue;
+                        if (oColor->Alpha == 0) continue;
 
-                        var final = new SKColor(blue: (byte)(255 * ((eyeColor.Blue / 255d) * (sColor->Red / 255d))),
-                                                green: (byte)(255 * ((eyeColor.Green / 255d) * (sColor->Red / 255d))),
-                                                red: (byte)(255 * ((eyeColor.Red / 255d) * (sColor->Red / 255d))),
-                                                alpha: sColor->Alpha);
-                        *oColor = SKPMColor.PreMultiply(final);
+                        var final = new SKColor(blue: (byte)(255 * ((eyeColor.Blue / 255d) * (oColor->Red / 255d))),
+                                                green: (byte)(255 * ((eyeColor.Green / 255d) * (oColor->Red / 255d))),
+                                                red: (byte)(255 * ((eyeColor.Red / 255d) * (oColor->Red / 255d))),
+                                                alpha: oColor->Alpha);
+                        *oColor = final;
                     }
                 }
             }
@@ -86,27 +82,23 @@ namespace PortraitBuilder.Engine
         /// </summary>
         public static SKBitmap ShadeHair(SKBitmap source, Hair hair)
         {
-            var output = new SKBitmap(source.Width, source.Height, source.ColorType, source.AlphaType);
-            output.Erase(SKColors.Transparent);
-
-            Debug.Assert(source.BytesPerPixel == 4 /* sizeof(BGRA) */);
+            var output = source.Copy();
 
             unsafe
             {
-                SKPMColor* sColor = (SKPMColor*)source.GetPixels().ToPointer();
-                SKPMColor* oColor = (SKPMColor*)output.GetPixels().ToPointer();
+                SKColor* oColor = (SKColor*)output.GetPixels().ToPointer();
 
                 for (int y = 0; y < source.Height; y++)
                 {
-                    for (int x = 0; x < source.Width; x++, sColor++, oColor++)
+                    for (int x = 0; x < source.Width; x++, oColor++)
                     {
-                        if (sColor->Alpha == 0) continue;
+                        if (oColor->Alpha == 0) continue;
 
-                        var lerp1 = Lerp(hair.Dark, hair.Base, Clamp(sColor->Green * 2d));
-                        var lerp2 = Lerp(lerp1, hair.Highlight, Clamp((sColor->Green - 128d) * 2));
-                        var final = lerp2.WithAlpha(sColor->Alpha);
+                        var lerp1 = Lerp(hair.Dark, hair.Base, Clamp(oColor->Green * 2d));
+                        var lerp2 = Lerp(lerp1, hair.Highlight, Clamp((oColor->Green - 128d) * 2));
+                        var final = lerp2.WithAlpha(oColor->Alpha);
 
-                        *oColor = SKPMColor.PreMultiply(final);
+                        *oColor = final;
                     }
                 }
             }
